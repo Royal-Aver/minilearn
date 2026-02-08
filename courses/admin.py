87d75c7936f5d56150a0e4546f1349cfd7b90728
Royal-Aver <royal_aver@rambler.ru>
2026-02-08 22:06:37 +0300

    feat(courses): Enable auto-slug generation in Course model admin and Simplify course creation in Django admin panel

--- courses/admin.py
+++ courses/admin.py
@@ -3,5 +3,11 @@ from django.contrib import admin
 from .models import Course, Category
 
 
-admin.site.register(Course)
-admin.site.register(Category)
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
