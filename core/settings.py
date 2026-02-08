0eb1c5a64ac7d4f448855a6c403502272babe8c0
Royal-Aver <royal_aver@rambler.ru>
2026-02-08 21:00:18 +0300

    feat: create core applications (users, courses, lessons, progress, quizzes)

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'courses.apps.CoursesConfig',
+    'users.apps.UsersConfig',
+    'lessons.apps.LessonsConfig',
+    'quizzes.apps.QuizzesConfig',
+    'progress.apps.ProgressConfig',
 ]
 
 MIDDLEWARE = [
